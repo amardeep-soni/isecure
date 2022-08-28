15e6cbfaa2111ad59466f7553acc1a97b5510d19
Amardeep soni <amardeep10as@gmail.com>
2022-08-28 15:48:24 +0545

    updated signup
    added navbar and signup form in signup.php

--- signup.php
+++ signup.php
@@ -12,7 +12,51 @@
 </head>
 
 <body>
-
+<!-- narbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Signup</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- signup form -->
+    <div class="container mt-5" style="max-width: 50%;">
+        <form action="/isecure/signup.php" method="POST">
+            <div class="form-group">
+                <label for="inputUsername">Username</label>
+                <input type="text" name="username" class="form-control" id="inputUsername">
+            </div>
+            <div class="form-group">
+                <label for="inputEmail">Email</label>
+                <input type="email" name="email" class="form-control" id="inputEmail">
+            </div>
+            <div class="form-group">
+                <label for="inputPassword">Password</label>
+                <input type="password" name="password" class="form-control" id="inputPassword">
+            </div>
+            <div class="form-group">
+                <label for="inputConformPassword">Conform Password</label>
+                <input type="password" name="conformPassword" class="form-control" id="inputConformPassword">
+            </div>
+            <button type="submit" name="signup" class="btn btn-primary">Signup</button>
+        </form>
+    </div>
     <script src="js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
