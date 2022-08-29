e9a68041b82dc6ad0ec0e77c4b2ac70be53aed47
Amardeep soni <amardeep10as@gmail.com>
2022-08-29 15:39:55 +0545

    putting the nav in another section and accessing the file in other

--- signup.php
+++ signup.php
@@ -50,28 +50,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <body>
     <!-- narbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/isecure/welcome.php">Welcome <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Signup</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/isecure/login.php">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/isecure/logout.php">Logout</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <?php
+    include 'components/nav.php';
+    ?>
 
     <!-- alerts when signup is success -->
     <?php
--- welcome.php
+++ welcome.php
@@ -22,28 +22,9 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
 
 <body>
     <!-- narbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Welcome <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/isecure/signup.php">Signup</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/isecure/login.php">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Logout</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <?php
+    include 'components/nav.php';
+    ?>
 
     <h1>Welcome <?php echo $_SESSION['username']; ?></h1>
 
