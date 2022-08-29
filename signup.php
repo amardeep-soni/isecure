b26527a67f0e49c7d2c39d32c11b73975db618a2
Amardeep soni <amardeep10as@gmail.com>
2022-08-29 15:17:33 +0545

    making the logout page

--- signup.php
+++ signup.php
@@ -67,7 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <a class="nav-link" href="/isecure/login.php">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Logout</a>
+                    <a class="nav-link" href="/isecure/logout.php">Logout</a>
                 </li>
             </ul>
         </div>
